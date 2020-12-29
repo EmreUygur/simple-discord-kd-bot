57d89ec356e88e0adfa3767d109ffe657b2b4cf7
EmreUygur <emre.uygur@outlook.com>
2020-12-29 23:02:20 +0300

    Initialize basic message analysing

--- main.js
+++ main.js
@@ -8,7 +8,18 @@ client.on("ready", () => {
 });
 
 client.on("message", (msg) => {
-  if (msg.content === prefix) {
+  // Prevent bot's own messages
+  if (msg.author.bot) return;
+  // Prevent other bots
+  if (msg.content.indexOf(prefix) !== 0) return;
+
+  // Splitting arguments
+  const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+
+  // Getting command from first argument
+  const command = args.shift().toLocaleLowerCase();
+
+  if (command === "ping") {
     msg.reply("pong");
   }
 });
