2721e584eec16bfe4a37052f1fa4dbdb6dab6c67
EmreUygur <emre.uygur@outlook.com>
2021-01-01 21:26:30 +0300

    Use command files

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs");
 const { token, prefix } = require("./config");
 
 const client = new Discord.Client();
+
+client.commands = new Enmap();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -19,20 +23,30 @@ client.on("message", (msg) => {
   // Getting command from first argument
   const command = args.shift().toLocaleLowerCase();
 
-  switch (command) {
-    case "ping":
-      msg.reply("pong");
-      break;
-    case "kill":
-      break;
-    case "death":
-      break;
-    case "help":
-      break;
-    default:
-      msg.reply("invalid command");
-      break;
+  // find command and its props by enmap
+  const cmd = client.commands.get(command);
+
+  // if no such command found
+  if (!cmd) {
+    msg.reply("Command not found");
+    return;
   }
+
+  // run command
+  cmd.run(client, msg, args);
+});
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error;
+
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    const props = require(`./commands/${file}`);
+    const cmd = file.split(".")[0];
+    console.log(`Loaded command: ${cmd}`);
+
+    client.commands.set(cmd, props);
+  });
 });
 
 client.login(token);
