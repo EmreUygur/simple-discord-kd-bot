163b11c7db92a350b17794f996459afcf4a16c21
EmreUygur <emre.uygur@outlook.com>
2021-01-10 20:07:57 +0300

    Design kill command algorithm

--- commands/kill.js
+++ commands/kill.js
@@ -1,8 +1,32 @@
 const { addRow } = require("../helpers/records.helper");
+const { findMemberByTag } = require("../helpers/guild.helper");
 
 // eslint-disable-next-line no-unused-vars
 exports.run = async (client, msg, args) => {
-  const row = [msg.author.tag, 1, 2];
+  if (args.length === 0) {
+    msg.reply("Target player is missing, please write");
+    return;
+  }
+
+  let tag = "";
+  args.forEach((arg) => {
+    tag += `${arg} `;
+  });
+
+  tag = tag.slice(0, -1);
+
+  const targetPlayer = await findMemberByTag(msg.guild, tag);
+
+  if (!targetPlayer) {
+    msg.reply("Target player is not found, did you write it correctly?");
+    return;
+  }
+
+  const row = {
+    date: new Date().getTime(),
+    player: msg.author.tag,
+    targetPlayer: tag,
+  };
   const result = await addRow(row);
 
   if (!result) {
